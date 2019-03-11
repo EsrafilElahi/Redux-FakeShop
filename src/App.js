e248ed6e983e0bce36d874f273ab1e3362701388
esrafil elahi <esrafil.elahi@gmail.com>
2019-03-11 14:38:45 +0330

    lint app.js file

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar'
-import CardDetail from './components/CardDetail'
-import Home from './pages/Home'
-import Basket from './pages/Basket'
-import Jewelery from './pages/Jewelery'
-import MenClothing from './pages/MenClothing'
-import WomenClothing from './pages/WomenClothing'
+import Navbar from "./components/Navbar";
+import CardDetail from "./components/CardDetail";
+import Home from "./pages/Home";
+import Basket from "./pages/Basket";
+import Jewelery from "./pages/Jewelery";
+import MenClothing from "./pages/MenClothing";
+import WomenClothing from "./pages/WomenClothing";
 
-
-import 'antd/dist/antd.css'
-import './App.css';
-
+import "antd/dist/antd.css";
+import "./App.css";
 
 function App() {
-
   return (
     <div>
       <Navbar />
 
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/basket' component={Basket} />
-        <Route path='/jewelery' component={Jewelery} />
-        <Route path='/men-clothing' component={MenClothing} />
-        <Route path='/women-clothing' component={WomenClothing} />
-        <Route path='/product/:id' component={CardDetail} />
+        <Route exact path="/" component={Home} />
+        <Route path="/basket" component={Basket} />
+        <Route path="/jewelery" component={Jewelery} />
+        <Route path="/men-clothing" component={MenClothing} />
+        <Route path="/women-clothing" component={WomenClothing} />
+        <Route path="/product/:id" component={CardDetail} />
         <Route>not found</Route>
       </Switch>
-
     </div>
-
   );
 }
 
